698c7cb1f9fec745eabff8197f3d7e02522ba2f0
Melocotonero Studio <aisayan68@gmail.com>
2023-12-02 22:38:34 +0100

    Criminal
    criminal cri criminal tu estilo tu flow mami muy criminal

---
+++ Comportamientos/Assets/Scripts/Tools/Vision.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+
+    public List<Transform> VisibleTriggers;
+
+    [SerializeField] LayerMask sceneMask;
+
+    private void Awake()
+    {
+        VisibleTriggers = new List<Transform>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        VisionTrigger visionTrigger = other.GetComponent<VisionTrigger>();
+
+
+        if (visionTrigger != null)
+        {
+            Vector3 direction = visionTrigger.transform.position - transform.position;
+            float raycastRange = direction.magnitude;
+            if (Physics.Raycast(transform.position, direction.normalized, out var hit, raycastRange,sceneMask))
+            {
+                VisibleTriggers.Remove(visionTrigger.Body);
+            }
+            else
+            {
+                if (!VisibleTriggers.Contains(visionTrigger.Body))
+                {
+                    VisibleTriggers.Add(visionTrigger.Body);
+                }
+            }
+
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        VisionTrigger visionTrigger = other.GetComponent<VisionTrigger>();
+
+
+        if (visionTrigger != null)
+        {
+            VisibleTriggers.Remove(visionTrigger.Body);
+        }
+    }
+}
